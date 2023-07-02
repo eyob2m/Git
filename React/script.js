a16a4a440ece78b6973b3a11a8994565d6949146
eyob2m <eyob2etemariam@gamil.com>
2023-07-03 02:37:43 +0300

    End of Day One

--- React/script.js
+++ React/script.js
@@ -1,3 +1,5 @@
+/*
+
 const page = (
 
 <nav>
@@ -16,4 +18,24 @@ const page = (
 
 )
 
-ReactDOM.render(page,document.getElementById("main"))
+ReactDOM.render(page,document.getElementById("main")) */
+
+const page = (
+
+    <nav>
+        <img src="logo.png" width="40px"></img>
+        <h1>Yenat</h1>
+    
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    
+    
+    
+    </nav>
+    
+    )
+
+    ReactDOM.render(page,document.getElementById("main"))
