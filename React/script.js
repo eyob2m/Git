5e3d96477ec9e19b9dc33ff61c3c94cde1ed91e4
eyob2m <eyob2etemariam@gmail.com>
2023-07-03 01:34:42 +0300

    React Day 1

---
+++ React/script.js
@@ -0,0 +1,19 @@
+const page = (
+
+<nav>
+
+    <h1>Yenat</h1>
+
+    <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+
+
+
+</nav>
+
+)
+
+ReactDOM.render(page,document.getElementById("main"))
