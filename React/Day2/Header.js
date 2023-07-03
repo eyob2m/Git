d53aa661c79d9390c0f4cd77464e9cbcc83e7ced
eyob2m <eyob2etemariam@gamil.com>
2023-07-03 19:28:59 +0300

    Ethiopia Web

---
+++ React/Day2/Header.js
@@ -0,0 +1,8 @@
+export default function Header(){
+
+return (
+<div>
+<h1>Ethiopia Interesting Facts</h1> </div>
+)
+
+}
---
+++ React/Test/nomore/src/mainb.jsx
@@ -0,0 +1,19 @@
+export default function Main() {
+
+return (
+
+    <div className="main">
+    <h2>Ethiopia</h2>
+    <ul className="okay">
+    <li>Ethiopia’s calendar will surprise you</li>
+    <li>It has the most UNESCO World Heritage Sites on the continent</li>
+    <li>Over 80 languages are spoken in Ethiopia</li>
+    <li>Over half of Africa’s mountains are in Ethiopia</li>
+    <li>The first African to win an Olympic gold medal was Ethiopian</li>
+    </ul>
+    
+    </div>
+
+)
+
+}
