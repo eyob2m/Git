3b2e15e0e61b69e64d74c0a84593f9c4e01bfa24
eyob2m <eyob2etemariam@gamil.com>
2023-07-04 03:01:31 +0300

    day 2 end

--- Digital Business Card/src/social.jsx
+++ Digital Business Card/src/social.jsx
@@ -6,6 +6,7 @@ return (
 <div className="fb"><a href="facebook.com/eyob2m"><i className="ri-facebook-circle-line"></i></a></div>
 <div className="ig"><a href="instagram.com/eyob2m"><i className="ri-instagram-line"></i></a></div>
 <div className="gh"><a href="github.com/eyob2m"><i className="ri-github-fill"></i></a></div>
+
 </div>
 
 )
